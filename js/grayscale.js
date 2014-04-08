957592bac763e690abb656f211128572d79f169f
moliri <leesha.maliakal@gmail.com>
2014-04-07 23:36:16 -0500

    more interface updates, map now displays Evanston for kicks

--- js/grayscale.js
+++ js/grayscale.js
@@ -21,7 +21,7 @@ $(function() {
 //Google Map Skin - Get more at http://snazzymaps.com/
 var myOptions = {
     zoom: 15,
-    center: new google.maps.LatLng(53.385873, -1.471471),
+    center: new google.maps.LatLng(42.0522432,-87.6785049),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
     styles: [{
